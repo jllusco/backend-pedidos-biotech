f263752ca596887b5db3fc587dc323d2d4e475a8
Juan Rosendo Llusco Catacora <juan.llusco@justicia.gob.bo>
2024-08-07 17:47:22 -0400

    update: inclusion de categoria en lista precio producto

--- app/Repository/ListaPrecioProductoRepository.php
+++ app/Repository/ListaPrecioProductoRepository.php
@@ -13,7 +13,7 @@ use App\Models\ListaPrecioProducto;
 class ListaPrecioProductoRepository
 {
     public function getByListaPrecio($idListaPrecio){
-        $query = ListaPrecioProducto::with('producto.registroSanitario');
+        $query = ListaPrecioProducto::with(['producto.registroSanitario','producto.categoria']);
         $query->where('lista_precio_id','=',$idListaPrecio);
         $query->orderBy('created_at','DESC');
 
