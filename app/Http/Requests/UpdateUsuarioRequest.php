29f438ece7299d889c6a31abf7338092759c5fb4
JUAN LLUSCO <jlluscos@gmail.com>
2024-11-28 19:49:53 -0400

    update: put usuario

--- app/Http/Requests/UpdateUsuarioRequest.php
+++ app/Http/Requests/UpdateUsuarioRequest.php
@@ -20,7 +20,6 @@ class UpdateUsuarioRequest extends FormRequest
     protected function prepareForValidation()
     {
         $this->merge([
-            'name'=>$this->usuario,
             'password'=>$this->contrasena,
             'numero_documento'=>$this->numeroDocumento,
             'primer_apellido'=>$this->primerApellido,
@@ -39,7 +38,6 @@ class UpdateUsuarioRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required','unique:users','min:4'],
             'numeroDocumento' => ['required','min:4'],
             'nombres' => ['required'],
             'primerApellido'=>['nullable'],
